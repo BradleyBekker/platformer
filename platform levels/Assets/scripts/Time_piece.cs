c509160251d76050fca3cb4f123c4390840c9c75
BradleyBekker <30977961+BradleyBekker@users.noreply.github.com>
2018-05-07 20:21:17 +0200

    files with the basic game element timer bell and end stage warp point (restarts level + basic assets doesnt look nice but works)

---
+++ platform levels/Assets/scripts/Time_piece.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time_piece : MonoBehaviour {
+   public GameObject world;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        world.GetComponent<world>().deathtimer += 5;
+        DestroyObject(gameObject);
+    }
+}
